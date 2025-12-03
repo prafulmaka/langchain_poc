ada9c682696dd9d00692ed63dfc0b02b31be9650
prafulmaka <praful.maka@gmail.com>
2025-12-03 01:55:58 -0800

    rag module

---
+++ rag_module.py
@@ -0,0 +1,9 @@
+class RAGModule:
+    def __init__(self):
+        self.text = "Hello World!"
+
+    def print_test(self):
+        print(self.text)
+
+if __name__ == "__main__":
+    RAGModule().print_test()
